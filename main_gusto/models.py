4750c6d793800d842f29d2b1b5b4192af6966380
Illya <illyatr25@gmail.com>
2021-02-24 15:36:24 +0200

    1

--- main_gusto/models.py
+++ main_gusto/models.py
@@ -1,3 +1,31 @@
 from django.db import models
+from uuid import uuid4
+import os
 
 # Create your models here.
+class Category(models.Model):
+    title = models.CharField(max_length=50)
+    category_order = models.IntegerField()
+    is_visible = models.BooleanField(default=True)
+    is_special = models.BooleanField(default=False)
+
+    def __str__(self):
+        return f'{self.title} : {self.category_order}'
+
+class Dish(models.Model):
+
+    def get_file_name_dishes(self, filename):
+        ext = filename.split('.')[-1]
+        filename = f'(uuid()).(ext)'
+        return os.path.join('images/dishes')
+    title = models.CharField(max_length=50)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+    description = models.CharField(max_length=300)
+    price = models.DecimalField(max_digits=7, decimal_places=2)
+    photo = models.ImageField(upload_to=get_file_name_dishes)
+    is_visible = models.BooleanField()
+
+
+
+    def __str__(self):
+        return f'{self.title} : {self.category}'
