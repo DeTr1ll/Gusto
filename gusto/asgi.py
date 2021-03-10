48dc72b6cf5bf0a90a4bd20caf31d2124139aa65
Illya <illyatr25@gmail.com>
2021-03-10 11:10:24 +0200

    1

--- gusto/asgi.py
+++ gusto/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for Gusto project.
+ASGI config for gusto project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Gusto.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'gusto.settings')
 
 application = get_asgi_application()
