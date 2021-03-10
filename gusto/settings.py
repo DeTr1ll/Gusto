15d5d548d059bad60b9240708d3c97d191641239
Illya <illyatr25@gmail.com>
2021-03-10 11:54:35 +0200

    1

--- gusto/settings.py
+++ gusto/settings.py
@@ -30,7 +30,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__fil
 
 # SECURITY WARNING: keep the secret key used in production secret!
 # SECRET_KEY = 'pyhvm-(tkndz9ag!k6=tu%c-=q*y0u3e&mr(_t4ksy9-4^_(wz'
-SECRET_KEY = SECRET_KEY
+
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
