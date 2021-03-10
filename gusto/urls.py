b3b2074bf778c876dd0f4d8f397506b844964d19
Illya <illyatr25@gmail.com>
2021-03-10 12:54:08 +0200

    1

--- gusto/urls.py
+++ gusto/urls.py
@@ -20,15 +20,14 @@ from gusto import settings
 from accounts.views import login_view, logout_view, register_view
 
 urlpatterns = [
-#     path('login/', login_view, name='login'),
-#     path('logout/', logout_view, name='logout'),
-#    path('register/', register_view, name='register'),
-#
-#     path('admin/', admin.site.urls),
-#     path('', include('main_gusto.urls')),
-#     path('menu/', include('menu_gusto.urls')),
-#     path('users_messages/', include('users_messages.urls')),
-#     path('menu_format/', include('menu_format.urls'))
+    path('login/', login_view, name='login'),
+    path('logout/', logout_view, name='logout'),
+    path('register/', register_view, name='register'),
+    path('admin/', admin.site.urls),
+    path('', include('main_gusto.urls')),
+    path('menu/', include('menu_gusto.urls')),
+    path('users_messages/', include('users_messages.urls')),
+    path('menu_format/', include('menu_format.urls'))
 
 
 ]
