ddaa805d39b8ac131249b9afe314e7e9e046f987
Illya <illyatr25@gmail.com>
2021-03-16 09:25:41 +0200

    1

--- Gusto/settings.py
+++ Gusto/settings.py
@@ -22,7 +22,7 @@ DB_USER = os.environ.get('DB_USER')
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
 
-BASE_DIR = os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
